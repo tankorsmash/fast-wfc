8502abc13a3d358af72580fbe2bd6feaa67c9b02
tankorsmash <tankorsmash@gmail.com>
2020-04-19 20:26:07 -0400

    dont use std namespace

--- example/src/lib/main.cpp
+++ example/src/lib/main.cpp
@@ -23,7 +23,6 @@
 #include "../include/image.hpp"
 #include "../include/utils.hpp"
 
-using namespace std;
 using namespace rapidxml;
 
 /**
@@ -44,7 +43,7 @@ int get_random_seed() {
  * Read the overlapping wfc problem from the xml node.
  */
 void read_overlapping_instance(rapidxml::xml_node<> *node) {
-    string name = rapidxml::get_attribute(node, "name");
+    std::string name = rapidxml::get_attribute(node, "name");
     unsigned N = stoi(rapidxml::get_attribute(node, "N"));
     bool periodic_output =
         (rapidxml::get_attribute(node, "periodic", "False") == "True");
@@ -57,7 +56,7 @@ void read_overlapping_instance(rapidxml::xml_node<> *node) {
     unsigned width = stoi(rapidxml::get_attribute(node, "width", "48"));
     unsigned height = stoi(rapidxml::get_attribute(node, "height", "48"));
 
-    cout << name << " started!" << endl;
+    std::cout << name << " started!" << std::endl;
     // Stop hardcoding samples
     const std::string image_path = "samples/" + name + ".png";
     Array2D<Color> m = read_image(image_path);
@@ -65,21 +64,23 @@ void read_overlapping_instance(rapidxml::xml_node<> *node) {
     if (m == NNN) {
         throw "Error while loading " + image_path;
     }
+
     OverlappingWFCOptions options = {
-        periodic_input, periodic_output, height, width, symmetry, ground, N};
+        periodic_input, periodic_output, height, width, symmetry, ground, N
+    };
     for (unsigned i = 0; i < screenshots; i++) {
         for (unsigned test = 0; test < 10; test++) {
             int seed = get_random_seed();
             OverlappingWFC<Color> wfc(m, options, seed);
             Array2D<Color> success = wfc.run();
             static Array2D<Color> NULL_COLOR_ARR = Array2D<Color>{0, 0};
+
             if (success != NULL_COLOR_ARR) {
-                write_image_png("results/" + name + to_string(i) + ".png", success);
-                cout << name << " finished!" << endl;
+                write_image_png("results/" + name + std::to_string(i) + ".png", success);
+                std::cout << name << " finished!" << std::endl;
                 break;
-            }
-            else {
-                cout << "ERROR: '" << name << "' failed!" << endl;
+            } else {
+                std::cout << "ERROR: '" << name << "' failed!" << std::endl;
             }
         }
     }
@@ -88,7 +89,7 @@ void read_overlapping_instance(rapidxml::xml_node<> *node) {
 /**
  * Transform a symmetry name into its Symmetry enum
  */
-Symmetry to_symmetry(const string &symmetry_name) {
+Symmetry to_symmetry(const std::string &symmetry_name) {
     if (symmetry_name == "X") {
         return Symmetry::X;
     }
@@ -113,9 +114,9 @@ Symmetry to_symmetry(const string &symmetry_name) {
 /**
  * Read the names of the tiles in the subset in a tiling WFC problem
  */
-unordered_set<string> read_subset_names(xml_node<> *root_node,
-    const string &subset) {
-    unordered_set<string> subset_names;
+std::unordered_set<std::string> read_subset_names(xml_node<> *root_node,
+    const std::string &subset) {
+    std::unordered_set<std::string> subset_names;
     xml_node<> *subsets_node = root_node->first_node("subsets");
     if (!subsets_node) {
         return{};
@@ -138,17 +139,17 @@ unordered_set<string> read_subset_names(xml_node<> *root_node,
 /**
  * Read all tiles for a tiling problem
  */
-unordered_map<string, Tile<Color>> read_tiles(xml_node<> *root_node,
-    const string &current_dir,
-    const string &subset,
+std::unordered_map<std::string, Tile<Color>> read_tiles(xml_node<> *root_node,
+    const std::string &current_dir,
+    const std::string &subset,
     unsigned size) {
-    unordered_set<string> subset_names =
+    std::unordered_set<std::string> subset_names =
         read_subset_names(root_node, subset);
-    unordered_map<string, Tile<Color>> tiles;
+    std::unordered_map<std::string, Tile<Color>> tiles;
     xml_node<> *tiles_node = root_node->first_node("tiles");
     for (xml_node<> *node = tiles_node->first_node("tile"); node;
         node = node->next_sibling("tile")) {
-        string name = rapidxml::get_attribute(node, "name");
+        std::string name = rapidxml::get_attribute(node, "name");
         if (subset_names.empty() &&
             subset_names.find(name) == subset_names.end()) {
             continue;
@@ -161,10 +162,10 @@ unordered_map<string, Tile<Color>> read_tiles(xml_node<> *root_node,
 
         static auto NNN = Array2D<Color>{0, 0};
         if (image == NNN) {
-            vector<Array2D<Color>> images;
+            std::vector<Array2D<Color>> images;
             for (unsigned i = 0; i < nb_of_possible_orientations(symmetry); i++) {
                 const std::string image_path =
-                    current_dir + "/" + name + " " + to_string(i) + ".png";
+                    current_dir + "/" + name + " " + std::to_string(i) + ".png";
                 Array2D<Color> image = read_image(image_path);
                 if (image == NNN) {
                     throw "Error while loading " + image_path;
@@ -195,26 +196,26 @@ unordered_map<string, Tile<Color>> read_tiles(xml_node<> *root_node,
  * A value {t1,o1,t2,o2} means that the tile t1 with orientation o1 can be
  * placed at the right of the tile t2 with orientation o2.
  */
-vector<tuple<string, unsigned, string, unsigned>>
+std::vector<std::tuple<std::string, unsigned, std::string, unsigned>>
 read_neighbors(xml_node<> *root_node) {
-    vector<tuple<string, unsigned, string, unsigned>> neighbors;
+    std::vector<std::tuple<std::string, unsigned, std::string, unsigned>> neighbors;
     xml_node<> *neighbor_node = root_node->first_node("neighbors");
     for (xml_node<> *node = neighbor_node->first_node("neighbor"); node;
         node = node->next_sibling("neighbor")) {
-        string left = rapidxml::get_attribute(node, "left");
-        string::size_type left_delimiter = left.find(" ");
-        string left_tile = left.substr(0, left_delimiter);
+        std::string left = rapidxml::get_attribute(node, "left");
+        std::string::size_type left_delimiter = left.find(" ");
+        std::string left_tile = left.substr(0, left_delimiter);
         unsigned left_orientation = 0;
-        if (left_delimiter != string::npos) {
-            left_orientation = stoi(left.substr(left_delimiter, string::npos));
+        if (left_delimiter != std::string::npos) {
+            left_orientation = stoi(left.substr(left_delimiter, std::string::npos));
         }
 
-        string right = rapidxml::get_attribute(node, "right");
-        string::size_type right_delimiter = right.find(" ");
-        string right_tile = right.substr(0, right_delimiter);
+        std::string right = rapidxml::get_attribute(node, "right");
+        std::string::size_type right_delimiter = right.find(" ");
+        std::string right_tile = right.substr(0, right_delimiter);
         unsigned right_orientation = 0;
-        if (right_delimiter != string::npos) {
-            right_orientation = stoi(right.substr(right_delimiter, string::npos));
+        if (right_delimiter != std::string::npos) {
+            right_orientation = stoi(right.substr(right_delimiter, std::string::npos));
         }
         neighbors.push_back(
             std::make_tuple(left_tile, left_orientation, right_tile, right_orientation));
@@ -226,52 +227,52 @@ read_neighbors(xml_node<> *root_node) {
  * Read an instance of a tiling WFC problem.
  */
 void read_simpletiled_instance(xml_node<> *node,
-    const string &current_dir)  {
-    string name = rapidxml::get_attribute(node, "name");
-    string subset = rapidxml::get_attribute(node, "subset", "tiles");
+    const std::string &current_dir)  {
+    std::string name = rapidxml::get_attribute(node, "name");
+    std::string subset = rapidxml::get_attribute(node, "subset", "tiles");
     bool periodic_output =
         (rapidxml::get_attribute(node, "periodic", "False") == "True");
     unsigned width = stoi(rapidxml::get_attribute(node, "width", "48"));
     unsigned height = stoi(rapidxml::get_attribute(node, "height", "48"));
 
-    cout << name << " " << subset << " started!" << endl;
+    std::cout << name << " " << subset << " started!" << std::endl;
 
-    ifstream config_file("samples/" + name + "/data.xml");
-    vector<char> buffer((istreambuf_iterator<char>(config_file)),
-        istreambuf_iterator<char>());
+    std::ifstream config_file("samples/" + name + "/data.xml");
+    std::vector<char> buffer((std::istreambuf_iterator<char>(config_file)),
+                             std::istreambuf_iterator<char>());
     buffer.push_back('\0');
     xml_document<> data_document;
     data_document.parse<0>(&buffer[0]);
     xml_node<> *data_root_node = data_document.first_node("set");
     unsigned size = stoi(rapidxml::get_attribute(data_root_node, "size"));
 
-    unordered_map<string, Tile<Color>> tiles_map =
+    std::unordered_map<std::string, Tile<Color>> tiles_map =
         read_tiles(data_root_node, current_dir + "/" + name, subset, size);
-    unordered_map<string, unsigned> tiles_id;
-    vector<Tile<Color>> tiles;
+    std::unordered_map<std::string, unsigned> tiles_id;
+    std::vector<Tile<Color>> tiles;
     unsigned id = 0;
-    for (pair<string, Tile<Color>> tile : tiles_map) {
+    for (std::pair<std::string, Tile<Color>> tile : tiles_map) {
         tiles_id.insert({tile.first, id});
         tiles.push_back(tile.second);
         id++;
     }
 
-    vector<tuple<string, unsigned, string, unsigned>> neighbors =
+    std::vector<std::tuple<std::string, unsigned, std::string, unsigned>> neighbors =
         read_neighbors(data_root_node);
-    vector<tuple<unsigned, unsigned, unsigned, unsigned>> neighbors_ids;
+    std::vector<std::tuple<unsigned, unsigned, unsigned, unsigned>> neighbors_ids;
     for (auto neighbor : neighbors) {
-        const string &neighbor1 = get<0>(neighbor);
-        const int &orientation1 = get<1>(neighbor);
-        const string &neighbor2 = get<2>(neighbor);
-        const int &orientation2 = get<3>(neighbor);
+        const std::string &neighbor1 = std::get<0>(neighbor);
+        const int &orientation1 = std::get<1>(neighbor);
+        const std::string &neighbor2 = std::get<2>(neighbor);
+        const int &orientation2 = std::get<3>(neighbor);
         if (tiles_id.find(neighbor1) == tiles_id.end()) {
             continue;
         }
         if (tiles_id.find(neighbor2) == tiles_id.end()) {
             continue;
         }
-        neighbors_ids.push_back(make_tuple(tiles_id[neighbor1], orientation1,
-            tiles_id[neighbor2], orientation2));
+        neighbors_ids.push_back(std::make_tuple(tiles_id[neighbor1], orientation1,
+                                                tiles_id[neighbor2], orientation2));
     }
 
     for (unsigned test = 0; test < 10; test++) {
@@ -283,11 +284,11 @@ void read_simpletiled_instance(xml_node<> *node,
         static auto NNN = Array2D<Color>{0, 0};
         if (success != NNN) {
             write_image_png("results/" + name + "_" + subset + ".png", success);
-            cout << name << " finished!" << endl;
+            std::cout << name << " finished!" << std::endl;
             break;
         }
         else {
-            cout << "failed!" << endl;
+            std::cout << "failed!" << std::endl;
         }
     }
 }
@@ -295,16 +296,16 @@ void read_simpletiled_instance(xml_node<> *node,
 /**
  * Read a configuration file containing multiple wfc problems
  */
-void read_config_file(const string &config_path)  {
-    ifstream config_file(config_path);
-    vector<char> buffer((istreambuf_iterator<char>(config_file)),
-        istreambuf_iterator<char>());
+void read_config_file(const std::string &config_path)  {
+    std::ifstream config_file(config_path);
+    std::vector<char> buffer((std::istreambuf_iterator<char>(config_file)),
+                             std::istreambuf_iterator<char>());
     buffer.push_back('\0');
     xml_document<> document;
     document.parse<0>(&buffer[0]);
 
     xml_node<> *root_node = document.first_node("samples");
-    string dir_path = get_dir(config_path) + "/" + "samples";
+    std::string dir_path = get_dir(config_path) + "/" + "samples";
     for (xml_node<> *node = root_node->first_node("overlapping"); node;
         node = node->next_sibling("overlapping")) {
         read_overlapping_instance(node);
